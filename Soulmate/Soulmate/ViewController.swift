28734965f513d776e6c126c4baea888a330d70fe
1145 <team_halves.0h@icloud.com>
2022-11-07 18:11:24 +0900

    [chore] 프로젝트 생성

---
+++ Soulmate/Soulmate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soulmate
+//
+//  Created by Hoen on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
