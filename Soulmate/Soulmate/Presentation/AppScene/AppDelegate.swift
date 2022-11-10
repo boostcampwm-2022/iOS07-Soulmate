1316a6966650e16ca21d74061427dbf549512f01
제리 <64826110+gwd0311@users.noreply.github.com>
2022-11-10 11:24:41 +0900

    [feat] Firebase 추가

--- Soulmate/Soulmate/Presentation/AppScene/AppDelegate.swift
+++ Soulmate/Soulmate/Presentation/AppScene/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -33,4 +33,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
