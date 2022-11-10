fda498a7442255281e396ecaf2012f2f1daddb7b
1145 <team_halves.0h@icloud.com>
2022-11-10 14:35:42 +0900

    [feat] 인증코드 입력 가이드 라벨 구현

--- Soulmate/Soulmate/Presentation/AuthScene/ViewController/CertificationNumberViewController.swift
+++ Soulmate/Soulmate/Presentation/AuthScene/ViewController/CertificationNumberViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class CertificationNumberViewController: UIViewController {
+    
+    private lazy var guideLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.text = "인증코드를 \n입력해주세요"
+        label.numberOfLines = 2
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10
+        let attr: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.paragraphStyle: paragraphStyle
+        ]
+        let attrString = NSMutableAttributedString(
+            string: label.text ?? "",
+            attributes: attr)
+        label.attributedText = attrString
+        label.font = UIFont.systemFont(ofSize: 22, weight: .init(700))
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureUI()
     }
 
+}
+
+// MARK: - Private functions
+
+private extension CertificationNumberViewController {
+    
+    func configureUI() {
+        
+        NSLayoutConstraint.activate([
+            self.guideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            self.guideLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
 
 #if DEBUG
