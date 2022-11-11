7f65bb99a3667516e8b42720a9ddd1b57fccc315
1145 <team_halves.0h@icloud.com>
2022-11-12 00:37:01 +0900

    [feat] ColorSet 추가

---
+++ Soulmate/Soulmate/Util/Extension/UIColor.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor.swift
+//  Soulmate
+//
+//  Created by Hoen on 2022/11/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundGrey = UIColor(named: "backgroundGrey")
+    static let borderPurple = UIColor(named: "borderPurple")
+    static let gradientBlue = UIColor(named: "gradientBlue")
+    static let gradientPurple = UIColor(named: "gradientPurple")
+    static let heartRed = UIColor(named: "heartRed")
+    static let labelDarkGrey = UIColor(named: "labelDarkGrey")
+    static let labelGrey = UIColor(named: "labelGrey")
+    static let lightPurple = UIColor(named: "lightPurple")
+    static let mainPurple = UIColor(named: "mainPurple")
+    static let mbtiPurple = UIColor(named: "mbtiPurple")
+    static let messagePurple = UIColor(named: "messagePurple")
+    static let symbolGrey = UIColor(named: "symbolGrey")
+    static let underlineGrey = UIColor(named: "underlineGrey")
+}
