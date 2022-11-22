6a7d30eeced069198b69aeffd05ff53a33deb2ac
termblur <termblur@gmail.com>
2022-11-22 18:18:15 +0900

    [chore] 상세화면 테스터

--- Soulmate/Soulmate/Presentation/HomeScene/ViewController/DetailViewController.swift
+++ Soulmate/Soulmate/Presentation/HomeScene/ViewController/DetailViewController.swift
@@ -48,6 +48,20 @@ final class DetailViewController: UIViewController {
         configureLayout()
         
         bind()
+        
+        // Test
+        viewModel = DetailViewModel(userInfo: RegisterUserInfo(
+            id: "1234",
+            gender: GenderType.male,
+            nickName: "테스트",
+            birthDay: Date(),
+            height: 175,
+            mbti: Mbti(innerType: InnerType.i, recognizeType: RecognizeType.n, judgementType: JudgementType.t, lifeStyleType: LifeStyleType.p),
+            smokingType: SmokingType.often,
+            drinkingType: DrinkingType.rarely,
+            aboutMe: "하아라어랴아 러야마얼 야라으마야 라야으라 야을 매야라 으랴 아르 얌로야 ㅡ 라얄오 ㅏ으 랴이라 야므 라야라 만아랴 으마야러 아먀아르 만아라 으망랑 러 ㅁㄴ아랑마라야르 하아 라어랴아러 야마얼야 라으마야 라야으라 야을매야라 으랴 아르 얌로야 ㅡ 라얄오 ㅏ으 랴이라 야므 라야라 만아 랴 으마야러 아먀아르 만아라 으망랑러 ㅁㄴ아랑마라야르 ",
+            imageList: ["heart", "logo", "Phone", "checkOn"]), distance: 11)
+
     }
 }
 
