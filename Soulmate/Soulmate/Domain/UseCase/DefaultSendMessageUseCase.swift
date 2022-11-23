2d95d0d3d00f9ce8f1b377a57c27912329caa3a5
1145 <team_halves.0h@icloud.com>
2022-11-24 00:55:37 +0900

    [feat] 최신 메세지 상태로 채팅목록 업데이트 기능 구현

--- Soulmate/Soulmate/Domain/UseCase/DefaultSendMessageUseCase.swift
+++ Soulmate/Soulmate/Domain/UseCase/DefaultSendMessageUseCase.swift
@@ -14,11 +14,14 @@ final class DefaultSendMessageUseCase: SendMessageUseCase {
     var messageToSend = CurrentValueSubject<String, Never>("")
     var sendButtonEnabled = CurrentValueSubject<Bool, Never>(false)
     
+    let db = Firestore.firestore()
     private let info: ChatRoomInfo
     private let uid = Auth.auth().currentUser?.uid
+    private let userDocRef: DocumentReference
     
     init(with info: ChatRoomInfo) {
         self.info = info
+        self.userDocRef = db.collection("UserDetailInfo").document("uid")
     }
     
     func updateMessage(_ text: String) {
@@ -32,8 +35,6 @@ final class DefaultSendMessageUseCase: SendMessageUseCase {
     }
     
     func sendMessage() {
-        let db = Firestore.firestore()
-        
         guard let documentId = info.documentId, let uid = self.uid else { return }
         
         if let docRef = try? db.collection("ChatRooms").document(documentId).collection("Messages").addDocument(
@@ -43,7 +44,24 @@ final class DefaultSendMessageUseCase: SendMessageUseCase {
                 userId: uid
             )
         ) {
-            docRef.updateData(["date" : FieldValue.serverTimestamp()])
+            Task {
+                do {
+                    try await docRef.updateData(["date": FieldValue.serverTimestamp()])
+                    let messageDoc = try await docRef.getDocument()                    
+                    
+                    guard let messageTime = messageDoc.data()?["date"] as? Timestamp else { return }
+                    
+                    try await db.collection("ChatRooms").document(documentId).updateData(
+                        [
+                            "lastMessage": messageToSend.value,
+                            "lastDate": messageTime
+                        ]
+                    )
+                    
+                } catch {
+                    print("Error update last data")
+                }
+            }
         } else {
             print("Error sending message document")
         }
