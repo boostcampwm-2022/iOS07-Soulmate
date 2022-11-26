9126fed4601be63027af0dd2a657a67f3b2f71b1
1145 <team_halves.0h@icloud.com>
2022-11-26 13:52:24 +0900

    [feat] 날짜 표시 개선

--- Soulmate/Soulmate/Util/Extension/Date+toString.swift
+++ Soulmate/Soulmate/Util/Extension/Date+toString.swift
@@ -12,35 +12,20 @@ extension Date {
     func passedTime() -> String {
         let now = Date.now
         let nowYear = Calendar.current.dateComponents([.year], from: now).year
-        let nowMonth = Calendar.current.dateComponents([.month], from: now).month
         let nowDay = Calendar.current.dateComponents([.day], from: now).day
-        let nowHour = Calendar.current.dateComponents([.hour], from: now).hour
-        let nowMinute = Calendar.current.dateComponents([.minute], from: now).minute
-        
+
         let messageYear = Calendar.current.dateComponents([.year], from: self).year
-        let messageMonth = Calendar.current.dateComponents([.month], from: self).month
         let messageDay = Calendar.current.dateComponents([.day], from: self).day
-        let messageHour = Calendar.current.dateComponents([.hour], from: self).hour
-        let messageMinute = Calendar.current.dateComponents([.minute], from: self).minute
         
-        let passedDay = nowDay! - messageDay!
+        if nowYear! > messageYear! {
+            return self.yyyyMMdd()
+        }
         
-        if passedDay > 0 {
-            if passedDay > 365 {
-                return passedWeeks(Int(ceil(Double(passedDay / 365))))
-            }
-            
-            if passedDay > 30 {
-                return passedWeeks(Int(ceil(Double(passedDay / 30))))
-            }
-            
-            if passedDay > 7 {
-                return passedWeeks(Int(ceil(Double(passedDay / 7))))
-            }
-            
-            return passedDays(passedDay)
-            
-        } else if nowDay! == messageDay! {
+        if nowDay! - messageDay! > 0 {
+            return self.Md()
+        }
+        
+        if nowDay! == messageDay! {
             return self.aHmm()
         }
         
@@ -55,19 +40,19 @@ extension Date {
         return dateFormatter.string(from: self)
     }
     
-    private func passedDays(_ days: Int) -> String {
-        return "\(days)일"
+    private func Md() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "M월 d일"
+        
+        return dateFormatter.string(from: self)
     }
     
-    private func passedWeeks(_ weeks: Int) -> String {
-        return "\(weeks)주"
-    }
-    
-    private func passedMonths(_ months: Int) -> String {
-        return "\(months)주"
-    }
-    
-    private func passedYears(_ years: Int) -> String {
-        return "\(years)주"
+    private func yyyyMMdd() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        
+        return dateFormatter.string(from: self)
     }
 }
