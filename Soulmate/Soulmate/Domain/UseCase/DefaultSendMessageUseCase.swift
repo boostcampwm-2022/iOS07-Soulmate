e4251d6805265d4ff8f5f7d86570f44af7a698bd
1145 <team_halves.0h@icloud.com>
2022-12-02 15:37:10 +0900

    [fix] 채팅방 1 안사라지던 버그 수정

--- Soulmate/Soulmate/Domain/UseCase/DefaultSendMessageUseCase.swift
+++ Soulmate/Soulmate/Domain/UseCase/DefaultSendMessageUseCase.swift
@@ -37,60 +37,46 @@ final class DefaultSendMessageUseCase: SendMessageUseCase {
     }
     
     func sendMessage() {
-        guard let documentId = info.documentId, let uid = self.uid else { return }
-        
+        guard let documentId = info.documentId, let uid = self.uid,
+              let othersId = info.userIds.first(where: { $0 != uid }) else { return }
         let chat = Chat(isMe: true, userId: uid, readUsers: [uid], text: messageToSend.value, date: Date.now, state: .sending)
         newMessage.send(chat)
-        
-        if let docRef = try? db
-            .collection("ChatRooms")
-            .document(documentId)
-            .collection("Messages")
-            .addDocument(
-                from: MessageToSendDTO(
-                    docId: documentId,
-                    text: messageToSend.value,
-                    userId: uid,
-                    readUsers: [uid],
-                    date: .init(date: Date.now)
-                ),
-                completion: { [weak self] err in
-                    if err != nil {
-                        var failedChat = chat
-                        failedChat.updateState(false, nil)
-                        self?.messageSended.send(failedChat)
-                    }
-                }
-            ) {
+        Task { [weak self] in
+            var unreadCount = try await db.collection("ChatRooms").document(documentId).getDocument().data(as: ChatRoomInfoDTO.self).unreadCount
+            // FIXME: force unwrapping 수정하기
+            unreadCount[othersId]! += 1
+            try await db.collection("ChatRooms").document(documentId).updateData(["unreadCount": unreadCount])
             
-            Task {
-                do {
-                    let messageDoc = try await docRef.getDocument()
-                    
-                    guard let messageTime = messageDoc.data()?["date"] as? Timestamp,
-                          let othersId = info.userIds.first(where: { $0 != uid }) else { return }
-                    
-                    try await db.collection("ChatRooms").document(documentId).updateData(
-                        [
-                            "lastMessage": messageToSend.value,
-                            "lastDate": messageTime
-                        ]
+            do {
+                let docRef = try db
+                    .collection("ChatRooms")
+                    .document(documentId)
+                    .collection("Messages")
+                    .addDocument(
+                        from: MessageToSendDTO(
+                            docId: documentId,
+                            text: messageToSend.value,
+                            userId: uid,
+                            readUsers: [uid],
+                            date: .init(date: Date.now)
+                        )
                     )
-                    
-                    var unreadCount = try await db.collection("ChatRooms").document(documentId).getDocument().data(as: ChatRoomInfoDTO.self).unreadCount
-                    
-                    // FIXME: force unwrapping 수정하기
-                    unreadCount[othersId]! += 1
-                    
-                    try await db.collection("ChatRooms").document(documentId).updateData(["unreadCount": unreadCount])
-                    
-                    var sendedChat = chat
-                    sendedChat.updateState(true, messageTime.dateValue())
-                    messageSended.send(sendedChat)
-                    
-                } catch {
-                    print("Error update last data")
-                }
+                let messageDoc = try await docRef.getDocument()
+                guard let messageTime = messageDoc.data()?["date"] as? Timestamp else { return }
+                try await db.collection("ChatRooms").document(documentId).updateData(
+                    [
+                        "lastMessage": messageToSend.value,
+                        "lastDate": messageTime
+                    ]
+                )
+                var sendedChat = chat
+                sendedChat.updateState(true, messageTime.dateValue())
+                self?.messageSended.send(sendedChat)
+            }
+            catch {
+                var failedChat = chat
+                failedChat.updateState(false, nil)
+                self?.messageSended.send(failedChat)
             }
         }
     }
